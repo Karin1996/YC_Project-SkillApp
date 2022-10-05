29e8877f72f68582cca751824696b923e0045fc1
woutvansteensel <woutvansteensel@gmail.com>
2022-10-05 15:09:56 +0200

    check it

--- Java/js/signin.js
+++ Java/js/signin.js
@@ -1,5 +1,6 @@
 function login() {
     let inputUserName = document.getElementById("username").value;
+    let inputPassword = document.getElementById("password").value;
     console.log(inputUserName)
     var url = `http://localhost:8082/users/name/${inputUserName}`;
     console.log("url var1 is: "+url);
@@ -11,8 +12,11 @@ function login() {
         document.getElementById('message')
             .innerHTML=userdata.name;
         console.log(userdata.id);
+        if(inputPassword == userdata.password)
+        {
         localStorage.setItem("keyId", userdata.id)
         location.href = "../Java/profile.html";
+        }
     }).catch((err)=>{
         console.log(err)
     })
