d78ea1e1cd860713afd990f0ee5362632e8b8188
faydewaal <fay_de_waal@hotmail.com>
2022-10-21 13:57:42 +0200

    wat een avontuur zeg

--- Java/js/GoalDetail.js
+++ Java/js/GoalDetail.js
@@ -5,6 +5,7 @@ var url = new URL(url_string);
 var paramValue = url.searchParams.get("deGoalId");
 console.log(paramValue)
 
+
 function haalGoalDetailOp() {
     fetch(`http://localhost:8082/goals/id/${paramValue}`)
         .then((data)=>{
@@ -29,6 +30,35 @@ function addGoalToDash() {
     location.href = "../Java/profile.html";
 }
 
+let subObj = {};
+function addSub(){
+
+    let subName = document.getElementById("name-of-sub").value;
+    let subDescript = document.getElementById("descript-sub").value;
+
+    subObj.nameOfGoal = subName;
+    subObj.description = subDescript;
+    const subJSON = JSON.stringify(subObj);
+
+    subRequest(subJSON)
+}
+
+function subRequest(json){
+    console.log(json);
+    fetch("http://localhost:8082/subGoal",
+        { headers: {
+                'Content-Type': 'application/json'}, method: 'POST', body: json
+        })
+}
+
+
+// function addSubToGoal() {
+//     fetch(`http://localhost:8082/goals/${paramValue}/${?}`, {
+//         method: 'PUT'
+//     })
+// }
+//
+//
 
 
 
